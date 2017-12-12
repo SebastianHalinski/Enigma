eb93241cd2c6b5c6a4a894e3f6472465c570105b
Sebastian <sebastian.halinski@gmail.com>
2017-12-12 14:40:55 +0100

    Implement encripter method in atbash module and add skeletons of rest methods

--- src/enigma/Atbash.java
+++ src/enigma/Atbash.java
@@ -10,3 +10,34 @@ public class Atbash implements EnigmaService {
 	public Atbash(String name){
 		this.name = name;
 	}
+
+    public String encipher(String text){
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String encoder = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        char[] values = text.toUpperCase().toCharArray();
+        String encripted_word = "";
+        for (int index = 0; index < values.length; index++) {
+            char letter = values[index];
+            int index_letter_encript = letters.indexOf(letter);
+            encripted_word += encoder.charAt(index_letter_encript);
+        }
+        return new String(encripted_word);
+    }
+
+
+    public String decipher(String text){
+    return "dec("+text+")";
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean isKeyRequired(){
+        return KEY_REQUIRED;
+    }
+
+    public void setKey(String key) {}
+
+
+    }
