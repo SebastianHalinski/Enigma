a0b4a713cc55f976d0bbd416c2a930e1c55a8cda
Kuba Mikołajczyk <qba600@gmail.com>
2017-12-13 14:40:35 +0100

    Fix setKey method - take args from terminal

--- src/enigma/Caesar.java
+++ src/enigma/Caesar.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 
 public class Caesar implements EnigmaService {
 
-	public static final boolean KEY_REQUIRED = false;
+	public static final boolean KEY_REQUIRED = true;
     private String name;
     private String key;
-    private Scanner userInput;
-    private String alphabet = "abcdefghijklmnoprstuwqrstxyzv";
 
     public Caesar(String name) {
 		this.name = name;
 	}
 
     public String encipher(String text) {
-        // take input from the user and set the key for encipher
-        userInputKey();
 		int shift = Integer.parseInt(this.key);
         String caesar_text = "";
         int len = text.length();
@@ -35,17 +31,9 @@ public class Caesar implements EnigmaService {
     public String decipher(String text) {
         String caesar_text = "";
         int len = text.length();
-        int shift;
-        
-        if(this.key == null) {
-            userInputKey();
-            shift = Integer.parseInt(this.key);
-            shift = shift * -1;
-        }
-        else {
-            shift = Integer.parseInt(this.key);
-            shift = shift * -1;
-        }
+        int shift = Integer.parseInt(this.key);
+        shift = shift * -1;
+    
 
         for(int x = 0; x < len; x++) {
             char c = (char)(text.charAt(x) + shift);
@@ -55,13 +43,6 @@ public class Caesar implements EnigmaService {
                 caesar_text += (char)(text.charAt(x) + shift); 
         }
         return caesar_text;
-    }
-
-    public void userInputKey() {
-        userInput = new Scanner(System.in);
-        System.out.print("Enter number to encipher/decipher the string: ");
-        String key = userInput.next();
-        setKey(key);
     }
 
     public String getName() {
