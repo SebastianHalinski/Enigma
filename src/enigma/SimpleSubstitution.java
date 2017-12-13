4ff6888a5e249e7f6fffba51bdbabec5a7f30ec0
kamilBoksa <kamil.boksa@gmail.com>
2017-12-13 11:43:34 +0100

    Add generateCipherAlphabet method in SimpleSubstitution class

--- src/enigma/SimpleSubstitution.java
+++ src/enigma/SimpleSubstitution.java
@@ -5,6 +5,7 @@ import services.EnigmaService;
 public class SimpleSubstitution implements EnigmaService {
 
 	public static final boolean KEY_REQUIRED = true;
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz ";
 	private String name;
 
 	public SimpleSubstitution(String name){
@@ -27,5 +28,25 @@ public class SimpleSubstitution implements EnigmaService {
         return KEY_REQUIRED;
     }
 
-    public void setKey(String key) {}
+    public void setKey(String key) {
+        System.out.println(key);
+    }
+
+    private String generateCipherAlphabet(String key){
+        key = key.toLowerCase();
+        Set<Character> uniqueCharacters = new LinkedHashSet<>();
+        for (int i = 0; i < key.length(); i++){
+            uniqueCharacters.add(key.charAt(i));
+        }
+        for (int i = 0; i < alphabet.length(); i++){
+            uniqueCharacters.add(alphabet.charAt(i));
+        }
+        String cipherAlphabet = new String();
+
+        for (Character element : uniqueCharacters){
+            cipherAlphabet += element;
+        }
+        return cipherAlphabet;
+
+    }
 }
