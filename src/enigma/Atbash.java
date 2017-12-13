a25f0ade5f8475d8e39a199c731aa83a4896a947
LMalko86 <lmalko2601@gmail.com>
2017-12-13 16:01:18 +0100

    Delete Atbash(name) method in Atbash class

--- src/enigma/Atbash.java
+++ src/enigma/Atbash.java
@@ -7,9 +7,6 @@ public class Atbash implements EnigmaService {
 	public static final boolean KEY_REQUIRED = false;
 	private String name;
 
-	public Atbash(String name){
-		this.name = name;
-	}
 
     public String encipher(String text){
         String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
