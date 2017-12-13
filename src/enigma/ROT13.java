edf1b085f95a4a465fe587c249b1b614a0622b14
LMalko86 <lmalko2601@gmail.com>
2017-12-13 15:43:39 +0100

    Delete method ROT13(name) in ROT13 class

--- src/enigma/ROT13.java
+++ src/enigma/ROT13.java
@@ -7,8 +7,6 @@ public class ROT13 implements EnigmaService {
 	public static final boolean KEY_REQUIRED = false;
 	private String name;
 
-	public ROT13(String name){
-		this.name = name;
 	}
 
 	public String encipher(String text){
