b32ab22851e68f1b697b0f54214160bcb621bc95
kamilBoksa <kamil.boksa@gmail.com>
2017-12-13 12:06:31 +0100

    Implement encipher method and add getKeyFromUser method in SimpleSubstitution class

--- src/enigma/SimpleSubstitution.java
+++ src/enigma/SimpleSubstitution.java
@@ -1,6 +1,9 @@
 package enigma;
 
 import services.EnigmaService;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.LinkedHashSet;
 
 public class SimpleSubstitution implements EnigmaService {
 
@@ -8,13 +11,23 @@ public class SimpleSubstitution implements EnigmaService {
     private String alphabet = "abcdefghijklmnopqrstuvwxyz ";
 	private String name;
     private String key;
+    private Scanner reader;
 
 	public SimpleSubstitution(String name){
 		this.name = name;
 	}
 
     public String encipher(String text){
-        return text;
+        getKeyFromUser();
+        String cipherAlphabet = generateCipherAlphabet(this.key);
+        String encoded = new String();
+
+        for(int i=0; i<text.length(); i++){
+            char temp = text.charAt(i);
+            int tempNums = new String(alphabet).indexOf(temp);
+            encoded += new String(cipherAlphabet).charAt(tempNums);
+        }
+        return encoded;
     }
 
     public String decipher(String text){
@@ -31,6 +44,15 @@ public class SimpleSubstitution implements EnigmaService {
 
     public void setKey(String key) {
         this.key = key;
+    }
+
+    private void getKeyFromUser(){
+        reader = new Scanner(System.in);
+        if (isKeyRequired()){
+            System.out.println("Enter key: ");
+            String key = reader.nextLine();
+            setKey(key);
+        }
     }
 
     private String generateCipherAlphabet(String key){
