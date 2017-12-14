857f79e26049677a21043569fb0cd971692e2e90
LMalko86 <lmalko2601@gmail.com>
2017-12-14 10:09:37 +0100

    Delete Caesar method and make name static final in Caesar

--- src/enigma/Caesar.java
+++ src/enigma/Caesar.java
@@ -6,12 +6,11 @@ import java.util.Scanner;
 public class Caesar implements EnigmaService {
 
 	public static final boolean KEY_REQUIRED = true;
+	public static final String name = "Caesar";
+
     private String name;
     private String key;
 
-    public Caesar(String name) {
-		this.name = name;
-	}
 
     public String encipher(String text) {
 		int shift = Integer.parseInt(this.key);
@@ -33,14 +32,14 @@ public class Caesar implements EnigmaService {
         int len = text.length();
         int shift = Integer.parseInt(this.key);
         shift = shift * -1;
-    
+
 
         for(int x = 0; x < len; x++) {
             char c = (char)(text.charAt(x) + shift);
             if (c > 'z')
                 caesar_text += (char)(text.charAt(x) - (26+shift));
             else
-                caesar_text += (char)(text.charAt(x) + shift); 
+                caesar_text += (char)(text.charAt(x) + shift);
         }
         return caesar_text;
     }
@@ -52,9 +51,9 @@ public class Caesar implements EnigmaService {
     public boolean isKeyRequired() {
 		return KEY_REQUIRED;
 	}
-    
+
     public void setKey(String key) {
-        
+
         if (isKeyOnlyNumbers(key)){
             this.key = key;
         }
@@ -65,7 +64,7 @@ public class Caesar implements EnigmaService {
     }
     private Boolean isKeyOnlyNumbers(String key){
         char[] chars = key.toCharArray();
- 
+
         for (char c : chars) {
             if(!Character.isDigit(c)){
                 return false;
