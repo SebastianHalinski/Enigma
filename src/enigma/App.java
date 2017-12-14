4b437e8c1ef7e72d6fcbf72aa271a505311c8447
LMalko86 <lmalko2601@gmail.com>
2017-12-14 09:34:22 +0100

    Delete unnecessary argument call in SimpleSubs register

--- src/enigma/App.java
+++ src/enigma/App.java
@@ -21,7 +21,7 @@ public class App {
         repo = new ServiceRepository();
         repo.register(new FakeEnigma("FakeEnigma"));
         repo.register(new ROT13("ROT13"));
-        repo.register(new SimpleSubstitution("SimpleSubstitution"));
+        repo.register(new SimpleSubstitution());
 
         if (args[0].equals("-l")) {
             for (String s : repo.listAll()) {
